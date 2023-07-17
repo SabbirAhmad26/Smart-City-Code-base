2385238f60d1ba97c7687ee56191a6a838bffe35
DangTran <72413423+DangTranQL@users.noreply.github.com>
2023-07-17 12:55:35 -0400

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,18 @@
+import math
+
+def circleRadius(b, c, d):
+  temp = c[0]**2 + c[1]**2
+  bc = (b[0]**2 + b[1]**2 - temp) / 2
+  cd = (temp - d[0]**2 - d[1]**2) / 2
+  det = (b[0] - c[0]) * (c[1] - d[1]) - (c[0] - d[0]) * (b[1] - c[1])
+
+  if abs(det) < 1.0e-10:
+    return None
+
+  # Center of circle
+  cx = (bc*(c[1] - d[1]) - cd*(b[1] - c[1])) / det
+  cy = ((b[0] - c[0]) * cd - (c[0] - d[0]) * bc) / det
+
+  radius = ((cx - b[0])**2 + (cy - b[1])**2)**.5
+
+  return radius, (cx, cy)
