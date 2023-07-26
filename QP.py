eb3c2a8c2147a1aab13d5d5edf41a7d5d2843c0a
Akua Dickson <118576545+akuadbu@users.noreply.github.com>
2023-07-26 11:49:22 -0400

    QP.py

--- QP.py
+++ QP.py
@@ -1,6 +1,7 @@
 import cvxpy as cp
 import numpy as np
 from scipy.integrate import odeint
+from OCT1 import OCT1
 
 
 class Robot:
@@ -14,9 +15,7 @@ class Robot:
         self.v_max = v_max
 
     def OCBF_SecondOrderDynamics(self, i, matrix, state):
-        ocpar = [-0.037707850046401380968287765285127, 2.134285324466861049452285051838,
-                 -33.881192418691944972270805953198, 260.54579661373139199242187335925,
-                 56.600557227222369626869908065326, 26.51967690466177119063767086725]
+        ocpar = OCT1(0, 0.1, 1.564)
         c = np.array(ocpar)
         x0 = np.array(state)
         eps = 10
@@ -25,8 +24,8 @@ class Robot:
 
         # reference Trajectory
         vd = 0.5 * c[0] * t ** 2 + c[1] * t + c[2]
+        print(vd)
         u_ref = c[0] * t + c[1]
-        print(u_ref)
 
         # Physical Limitations on velocity
         b_vmax = self.v_max - x0[1]
@@ -72,10 +71,7 @@ class Robot:
                     continue
                 else:
                     d1 = matrix[row_index][3] - matrix[row_index][1]
-                    print("d1 is:", d1)
                     d2 = state[row_index + 2] - state[0]
-                    print("d2 is:", d2)
-
                 L = state[row_index + 2]
 
                 v0 = matrix[row_index][2]
@@ -126,24 +122,21 @@ class Robot:
             return dx
 
         u = (solveQP(2),)
-        print(u)
         x = np.zeros(2)
         t_start = 0
         t_end = 0.1
         t_span = (t_start, t_end)
 
         solution = odeint(second_order_model, x0[0:2], t_span, args=u)
-        print(solution)
 
         rt = [solution[-1][0], solution[-1][1]]
         print(rt)
 
 
-my_robot = Robot(-3, 3, 1.8, 1.8, 10, 0, 15)
+my_robot = Robot(-1, 1, 0.18, 0.18, 0.1, 0, 1)
 
-my_robot.OCBF_SecondOrderDynamics(495, np.array([[5, 492.1331, 15, -1], [6, 477.5007, 15, 312.75],
-                                            [8, 431.7498, 15, 311], [12, 341.7498, 15, 310.3498],
-                                            [15, 320.3925, 15, 316.25]]),
-                                  [295.3192, 13.5140, 0.2898, 305.9653, 310.3498, 309.6779, 314.0624])
+my_robot.OCBF_SecondOrderDynamics(1, np.array([[3, 1.082444, 0, -1], [2, 2.25106, 0.5, 0.4373],
+                                               [4, 1.1569, 0.5, 1.5314]]),
+                                  [0.503779, 0.5, 0.2898, 1.061228, 1.061228, 1.061228])
 
 # The 5 additional columns in the state list for our car is the distance of our car from each of the merging points!
