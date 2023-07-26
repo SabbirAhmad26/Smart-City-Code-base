be7b8ca7b8720038e74f0043ee67c11924f21a18
DangTran <72413423+DangTranQL@users.noreply.github.com>
2023-07-26 16:53:43 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -14,6 +14,7 @@ left = [
 ]
 
 left_circle, right_circle = circle.circle_lst()
+border = [(-1.301, 1.363), (-3.255, 3.489), (-1.088, 5.619), (-0.691, 2.861)]
 
 merging_pts = {
     "straight_A": [(-0.579, 2.94), (-0.604, 4.035), (-0.608, 4.447)],
@@ -30,83 +31,8 @@ merging_pts = {
     "left_D": [(-0.604, 4.035), (-1.078, 3.468), (-1.72, 2.91), (-1.72, 2.516)]
 }
 
-def signed_dist(pos, path):
+def merging_dst(pos, path):
     x,y = pos
-    if path == "straight_A":
-        return x - turning_pts_right[0][0]
-    elif path == "straight_B":
-        return y - turning_pts_right[1][1]
-    elif path == "straight_C":
-        return x - turning_pts_right[2][0]
-    elif path == "straight_D":
-        return y - turning_pts_right[3][1]
-
-    elif path == "right_A":
-        if y < turning_pts_right[0][1]:
-            return x - turning_pts_right[0][0]
-        else:
-            if x > right[0][0]:
-                return -(y - right[0][1])
-            else:
-                return -(math.sqrt((x-right_circle[0][1][0])**2 + (y-right_circle[0][1][1])**2) - right_circle[0][0])
-    elif path == "right_B":
-        if x < turning_pts_right[1][0]:
-            return -(y - turning_pts_right[1][1])
-        else:
-            if y < right[1][1]:
-                return x - right[1][0]
-            else:
-                return -(math.sqrt((x-right_circle[1][1][0])**2 + (y-right_circle[1][1][1])**2) - right_circle[1][0])
-    elif path == "right_C":
-        if y > turning_pts_right[2][1]:
-            return x - turning_pts_right[2][0]
-        else:
-            if x < right[2][0]:
-                return -(y - right[2][1])
-            else:
-                return -(math.sqrt((x-right_circle[2][1][0])**2 + (y-right_circle[2][1][1])**2) - right_circle[2][0])
-    elif path == "right_D":
-        if x > turning_pts_right[3][0]:
-            return -(y - turning_pts_right[3][1])
-        else:
-            if y > right[3][1]:
-                return x - right[3][0]
-            else:
-                return -(math.sqrt((x-right_circle[3][1][0])**2 + (y-right_circle[3][1][1])**2) - right_circle[3][0])
-
-    elif path == "left_A":
-        if y < turning_pts_left[0][1]:
-            return x - turning_pts_left[0][0]
-        else:
-            if x < left[0][0]:
-                return y - left[0][1]
-            else:
-                return math.sqrt((x-left_circle[0][1][0])**2 + (y-left_circle[0][1][1])**2) - left_circle[0][0]
-    elif path == "left_B":
-        if x < turning_pts_left[1][0]:
-            return y - turning_pts_left[1][1]
-        else:
-            if y > left[1][1]:
-                return x - left[1][0]
-            else:
-                return math.sqrt((x-left_circle[1][1][0])**2 + (y-left_circle[1][1][1])**2) - left_circle[1][0]
-    elif path == "left_C":
-        if y > turning_pts_left[2][1]:
-            return x - turning_pts_left[2][0]
-        else:
-            if x > left[2][0]:
-                return y - left[2][1]
-            else:
-                return math.sqrt((x-left_circle[2][1][0])**2 + (y-left_circle[2][1][1])**2) - left_circle[2][0]
-    elif path == "left_D":
-        if x > turning_pts_left[3][0]:
-            return y - turning_pts_left[3][1]
-        else:
-            if y < left[3][1]:
-                return x - left[3][0]
-            else:
-                return math.sqrt((x-left_circle[3][1][0])**2 + (y-left_circle[3][1][1])**2) - left_circle[3][0]
-    
     index = ord(path[-1])-65
     if "straight" in path:
         if len(merging_pts[path]) != 0:
@@ -116,9 +42,9 @@ def signed_dist(pos, path):
     elif "right" in path:
         if len(merging_pts[path]) != 0:
             if ("A" in path and y < turning_pts_right[0][1]) or ("C" in path and y > turning_pts_right[2][1]):
-                d = abs(y-turning_pts_right[index][1]) + arc_length(right_circle[index], turning_pts_right[index], merging_pts[path])
+                d = abs(y-turning_pts_right[index][1]) + arc_length(right_circle[index], turning_pts_right[index], merging_pts[path][0])
             elif ("B" in path and x < turning_pts_right[1][0]) or ("D" in path and x > turning_pts_right[3][0]):
-                d = abs(x-turning_pts_right[index][0]) + arc_length(right_circle[index], turning_pts_right[index], merging_pts[path])
+                d = abs(x-turning_pts_right[index][0]) + arc_length(right_circle[index], turning_pts_right[index], merging_pts[path][0])
             else:
                 d = arc_length(right_circle[index], (x,y), merging_pts[path])
             
@@ -139,4 +65,110 @@ def signed_dist(pos, path):
             
             if d <= 0.01:
                 merging_pts.pop(0)
+
+def signed_dist(pos, path):
+    x,y = pos
+    stop = False
+    pid = ["straight", "right", "left"]
+    
+    d = merging_dst(pos, path)
+    
+    if path == "straight_A":
+        if abs(y-border[2][1]) <= 0.01:
+            stop = True
+        return x - turning_pts_right[0][0], d, stop, pid[0]
+    elif path == "straight_B":
+        if abs(x-border[3][0]) <= 0.01:
+            stop = True
+        return y - turning_pts_right[1][1], d, stop, pid[0]
+    elif path == "straight_C":
+        if abs(y-border[0][1]) <= 0.01:
+            stop = True
+        return x - turning_pts_right[2][0], d, stop, pid[0]
+    elif path == "straight_D":
+        if abs(x-border[1][0]) <= 0.01:
+            stop = True
+        return y - turning_pts_right[3][1], d, stop, pid[0]
+
+    elif path == "right_A":
+        if abs(x-border[3][0]) <= 0.01:
+            stop = True
+        if y < turning_pts_right[0][1]:
+            return x - turning_pts_right[0][0], d, stop, pid[0]
+        else:
+            if x > right[0][0]:
+                return -(y - right[0][1]), d, stop, pid[0]
+            else:
+                return -(math.sqrt((x-right_circle[0][1][0])**2 + (y-right_circle[0][1][1])**2) - right_circle[0][0]), d, stop, pid[1]
+    elif path == "right_B":
+        if abs(y-border[0][1]) <= 0.01:
+            stop = True
+        if x < turning_pts_right[1][0]:
+            return -(y - turning_pts_right[1][1]), d, stop, pid[0]
+        else:
+            if y < right[1][1]:
+                return x - right[1][0], d, stop, pid[0]
+            else:
+                return -(math.sqrt((x-right_circle[1][1][0])**2 + (y-right_circle[1][1][1])**2) - right_circle[1][0]), d, stop, pid[1]
+    elif path == "right_C":
+        if abs(x-border[1][0]) <= 0.01:
+            stop = True
+        if y > turning_pts_right[2][1]:
+            return x - turning_pts_right[2][0], d, stop, pid[0]
+        else:
+            if x < right[2][0]:
+                return -(y - right[2][1]), d, stop, pid[0]
+            else:
+                return -(math.sqrt((x-right_circle[2][1][0])**2 + (y-right_circle[2][1][1])**2) - right_circle[2][0]), d, stop, pid[1]
+    elif path == "right_D":
+        if abs(y-border[2][1]) <= 0.01:
+            stop = True
+        if x > turning_pts_right[3][0]:
+            return -(y - turning_pts_right[3][1]), d, stop, pid[0]
+        else:
+            if y > right[3][1]:
+                return x - right[3][0], d, stop, pid[0]
+            else:
+                return -(math.sqrt((x-right_circle[3][1][0])**2 + (y-right_circle[3][1][1])**2) - right_circle[3][0]), d, stop, pid[1]
+
+    elif path == "left_A":
+        if abs(x-border[1][0]) <= 0.01:
+            stop = True
+        if y < turning_pts_left[0][1]:
+            return x - turning_pts_left[0][0], d, stop, pid[0]
+        else:
+            if x < left[0][0]:
+                return y - left[0][1], d, stop, pid[0]
+            else:
+                return math.sqrt((x-left_circle[0][1][0])**2 + (y-left_circle[0][1][1])**2) - left_circle[0][0], d, stop, pid[2]
+    elif path == "left_B":
+        if abs(y-border[2][1]) <= 0.01:
+            stop = True
+        if x < turning_pts_left[1][0]:
+            return y - turning_pts_left[1][1], d, stop, pid[0]
+        else:
+            if y > left[1][1]:
+                return x - left[1][0], d, stop, pid[0]
+            else:
+                return math.sqrt((x-left_circle[1][1][0])**2 + (y-left_circle[1][1][1])**2) - left_circle[1][0], d, stop, pid[2]
+    elif path == "left_C":
+        if abs(x-border[3][0]) <= 0.01:
+            stop = True
+        if y > turning_pts_left[2][1]:
+            return x - turning_pts_left[2][0], d, stop, pid[0]
+        else:
+            if x > left[2][0]:
+                return y - left[2][1], d, stop, pid[0]
+            else:
+                return math.sqrt((x-left_circle[2][1][0])**2 + (y-left_circle[2][1][1])**2) - left_circle[2][0], d, stop, pid[2]
+    elif path == "left_D":
+        if abs(y-border[0][1]) <= 0.01:
+            stop = True
+        if x > turning_pts_left[3][0]:
+            return y - turning_pts_left[3][1], d, stop, pid[0]
+        else:
+            if y < left[3][1]:
+                return x - left[3][0], d, stop, pid[0]
+            else:
+                return math.sqrt((x-left_circle[3][1][0])**2 + (y-left_circle[3][1][1])**2) - left_circle[3][0], d, stop, pid[2]
             
