62c452a17b6472f2f54ca27fe6b225645f8e386c
Akua Dickson <118576545+akuadbu@users.noreply.github.com>
2023-08-04 17:12:47 -0400

    Update QP.py

--- QP.py
+++ QP.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from cvxopt.solvers import qp
 from scipy.integrate import odeint
@@ -16,7 +15,7 @@ class Robot:
         self.v_max = v_max
 
     def OCBF_SecondOrderDynamics(self, i, matrix_const, state):
-        ocpar = [-0.775514512237531, 1.35826989127805, 0.100000000000000, 0, 1.75144355114535, 1.28946652089692];
+        ocpar = [-0.593787660013256, 1.41421356237309, 0, 0, 2.38168230431317, 1.68410370801184];
         c = np.array(ocpar)
         x0 = np.array(state)
         eps = 10
@@ -70,30 +69,32 @@ class Robot:
                     continue
                 else:
                     d1 = matrix_const[row_index][3]
-                    d2 = state[row_index + 2] + state[0]
-                L = state[row_index + 2]
+                    d2 = state[row_index + 2]
+                # L = state[row_index + 2] + state[0]
+                L = 2.715
 
                 v0 = matrix_const[row_index][2]
 
                 bigPhi = self.phiLateral * x0[0] / L
-                h = d2 - d1 - bigPhi * x0[1]
+                h = d2 - d1 - bigPhi * x0[1] - self.deltaSafetyDistance
 
                 uminValue = abs(self.u_min)
                 hf = d2 - d1 - 0.5 * (v0 - x0[1]) ** 2 / uminValue - self.phiLateral * v0 * (
                         x0[0] + 0.5 * (x0[1] ** 2 - v0 ** 2) / uminValue) / L
 
-                if x0[1] <= v0 or hf < 0:
-                    LgB = bigPhi
-                    LfB = v0 - x0[1] - self.phiLateral * x0[1] ** 2 / L
-                    if LgB != 0:
-                        A = np.append(A, [[LgB, 0]], axis=0)
-                        b = np.append(b, [LfB + h])
-                else:
-                    LgB = self.phiLateral * v0 * x0[1] / uminValue / L - (v0 - x0[1]) / uminValue
-                    LfB = v0 - x0[1] - self.phiLateral * v0 * x0[1] / L
-                    if LgB != 0:
-                        A = np.append(A, [[LgB, 0]], axis=0)
-                        b = np.append(b, [LfB + hf])
+                # if x0[1] <= v0 or hf < 0:
+                LgB = bigPhi
+                LfB = v0 - x0[1] - self.phiLateral * x0[1] ** 2 / L
+                if LgB != 0:
+                    stop = 1
+                    A = np.append(A, [[LgB, 0]], axis=0)
+                    b = np.append(b, [LfB + h])
+                # else:
+                #     LgB = self.phiLateral * v0 * x0[1] / uminValue / L - (v0 - x0[1]) / uminValue
+                #     LfB = v0 - x0[1] - self.phiLateral * v0 * x0[1] / L
+                #     if LgB != 0:
+                #         A = np.append(A, [[LgB, 0]], axis=0)
+                #         b = np.append(b, [LfB + hf])
 
             H = matrix([[1, 0], [0, psc]])
             f = matrix([[-u_ref], [0]]).trans()
@@ -115,18 +116,21 @@ class Robot:
             return dx
 
         u = solveQP()
-        x = np.zeros(2)
-        t_start = 0
-        t_end = 0.1
-        t_span = (t_start, t_end)
+        # x = np.zeros(2)
+        # t_start = 0
+        # t_end = 0.1
+        # t_span = (t_start, t_end)
 
-        solution = odeint(second_order_model, x0[0:2], t_span, args=u)
+        # solution = odeint(second_order_model, x0[0:2], t_span, args=u)
 
-        rt = [solution[-1][0], solution[-1][1]]
+        # rt = [solution[-1][0], solution[-1][1]]
+        print(u[0])
 
 
 my_robot = Robot(-1, 1, 0.18, 0.18, 0.1, 0, 1)
-
-my_robot.OCBF_SecondOrderDynamics(1, np.array([[-10, 10, 0.5, -1], [789, .25106, 0, 1.0758]]
+L = 2.715
+d2 = 0.7
+d1 = 0.579
+my_robot.OCBF_SecondOrderDynamics(1, np.array([[-1, 2.2, 0, -1], [789, 1.298, 0, d1]]
                                               ),
-                                  [0.503779, 0.05, 1, 1.085])
+                                  [L - d2, 0.02, -1, d2])
